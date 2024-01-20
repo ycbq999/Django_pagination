cefc56050883b067641d64db44b3c1f7af5e596c
ycbq999 <ycbq999@hotmail.com>
2024-01-19 22:10:58 -0800

    all completed

--- mysite/newapp/views.py
+++ mysite/newapp/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from .models import Movies
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 def movie_list(request):
     movie_objects = Movies.objects.all()
+
+    movie_name = request.GET.get('movie_name')
+
+    if movie_name != '' and movie_name is not None:
+        movie_objects = movie_objects.filter(name__icontains=movie_name)
+
+
+
+    paginator = Paginator(movie_objects, 5)
+    page = request.GET.get('page')
+    movie_objects = paginator.get_page(page)
+
     return render(request, 'newapp/movie_list.html', {'movie_objects': movie_objects})    
